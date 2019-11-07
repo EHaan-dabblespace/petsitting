b59e56e86d021971ba090ba9f638dbcc1adc546d
evyhaan <evy.haan@gmail.com>
2019-11-07 11:52:02 -0800

    separate login from registration and set correct homepage and redirects

--- petsitter_app/urls.py
+++ petsitter_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('pets/', PetListView.as_view(), name='pet_list'),
+    path('', PetListView.as_view(), name='pet_list'),
     path('pets/create', PetCreateView.as_view(), name='pet_create'),
     path('pets/<int:pk>', PetDetailView.as_view(), name='pet_detail'),
     path('pets/<int:pk>/update', PetUpdateView.as_view(), name='pet_update'),
